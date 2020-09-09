a61b596f5f959dc1dc3dcb4f975318d2778292cb
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-09-09 15:48:35 +0200

    theme and early routes

--- App/js/App.js
+++ App/js/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+import {ThemeProvider} from "@material-ui/core/styles";
+import * as ROUTES from "./constants/routes";
+import SignInPage from "./components/SignInPage";
+import SignUpPage from "./components/SignUpPage";
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: blueGrey[900]
+        },
+        secondary: {
+            main: "#ff3d00"
+        },
+    },
+})
 
 const App = () => {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Navbar/>
+                <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
+                <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+            </BrowserRouter>
+        </ThemeProvider>
+
+
     );
 };
 

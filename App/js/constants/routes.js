8d67554fded8095bb6b440a82cac8cb71dfc1b26
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-09-10 12:40:42 +0200

    added new routes

--- App/js/constants/routes.js
+++ App/js/constants/routes.js
@@ -1,3 +1,6 @@
 export const HOME = "/";
 export const SIGN_UP = "/signup";
 export const SIGN_IN = "/signin";
+export const ACCOUNT = "/account";
+export const PASSWORD_FORGET = "/pw-reset"
+export const ADMIN = "/admin"

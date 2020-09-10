b0c311c832d8acd194825bde4e30d9c76a691706
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-09-10 12:36:39 +0200

    added conditional rendering

--- App/js/components/Navbar.js
+++ App/js/components/Navbar.js
@@ -3,31 +3,61 @@ import {Toolbar} from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import * as ROUTES from "../constants/routes";
 import {Link} from "react-router-dom";
+import SignOut from "./Firebase/SignOut";
+import AuthUserContext from "./SessionContext";
 
-const Navbar = () => {
-    return (
-        <>
-            <AppBar positon="sticky">
-                <Toolbar>
-                    <ul>
-                        <li >
-                            <Link to={ROUTES.SIGN_IN}>Sign in</Link>
-                        </li>
+const Navbar = () => (
+    <>
+        <AppBar positon="sticky">
+            <Toolbar>
+                <div>
+                    <AuthUserContext.Consumer>
+                        {authUser => authUser ? <NavbarAuthorized/> : <NavbarNonAuthorized/>}
+                    </AuthUserContext.Consumer>
 
-                        <li>
-                            <Link to={ROUTES.HOME}>Home</Link>
-                        </li>
-
-                        <li>
-                            <Link to={ROUTES.SIGN_UP}>Sign up</Link>
-                        </li>
-
-                    </ul>
-                </Toolbar>
-            </AppBar>
-            <Toolbar/>
+                </div>
+            </Toolbar>
+        </AppBar>
+        <Toolbar/>
         </>
+);
+
+
+
+const NavbarAuthorized = () => {
+    return (
+        <ul>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+                <SignOut/>
+            </li>
+        </ul>
     );
 };
+
+
+const NavbarNonAuthorized = () => {
+    return (
+        <ul>
+            <li >
+                <Link to={ROUTES.SIGN_IN}>Sign in</Link>
+            </li>
+
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+
+            <li>
+                <Link to={ROUTES.SIGN_UP}>Sign up</Link>
+            </li>
+        </ul>
+    );
+};
+
 
 export default Navbar;

6a8a4ed39809183e5d02cdb3c7fe48d746335b5d
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-09-12 18:12:48 +0200

    added early modal

--- App/js/components/Navbar.js
+++ App/js/components/Navbar.js
@@ -9,7 +9,7 @@ import Typography from "@material-ui/core/Typography";
 import HomeIcon from '@material-ui/icons/Home';
 import IconButton from "@material-ui/core/IconButton";
 import {makeStyles} from "@material-ui/core/styles";
-import AddBoxRoundedIcon from '@material-ui/icons/AddBoxRounded';
+import FileUploadModal from "./FileUploadModal";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -47,9 +47,9 @@ const Navbar = () => {
 
 const NavbarAuthorized = () => {
     return (
-        <div>
+        <div className="app__menu">
             <Link color="secondary" to={ROUTES.HOME}><IconButton><HomeIcon/>Home</IconButton></Link>
-            <IconButton><AddBoxRoundedIcon/>Upload</IconButton>
+            <FileUploadModal/>
             <Link to={ROUTES.ACCOUNT}>Account</Link>
             <SignOut/>
         </div>

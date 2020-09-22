43a65b8b5098cc6a2a06a1ce6257707648ba1724
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-09-22 18:15:38 +0200

    early conditional permissions

---
+++ App/js/components/PermissionsContext.js
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react';
+import * as ROUTES from "../constants/routes";
+import {withFirebase} from "./Firebase/context";
+import {useHistory} from "react-router-dom";
+import AuthUserContext from "./SessionContext";
+
+
+const withPermissions = condition => Component => {
+    const PermissionsContext = (props) => {
+        const history = useHistory();
+        useEffect(() => {
+            const listener = props.firebase.auth.onAuthStateChanged(
+                authUser => {
+                    if (!condition(authUser)) {
+                        history.push(ROUTES.SIGN_IN);
+                    }
+                }
+            )
+            return () => {
+                listener();
+            }
+        });
+
+        return (
+            <AuthUserContext.Consumer>
+                {authUser => condition(authUser) ? <Component {...props} /> : null
+                }
+            </AuthUserContext.Consumer>
+        )
+    }
+    return withFirebase(PermissionsContext);
+}
+
+export default withPermissions;

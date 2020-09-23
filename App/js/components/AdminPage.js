f32ccc8c5ef61f89871dbbd3c01c55f8bb363b63
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-09-23 14:19:01 +0200

    display a list of users

--- App/js/components/AdminPage.js
+++ App/js/components/AdminPage.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PermissionsContext from "./PermissionsContext";
+import {Typography} from "@material-ui/core";
 
-const AdminPage = () => {
+const AdminPage = (props) => {
+
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        let mounted = true;
+        props.firebase.database.collection("users").onSnapshot(snapshot => {
+            if(mounted) {
+                setUsers(snapshot.docs);
+            }
+        })
+
+        return () => {
+            mounted = false;
+        }
+    },[]);
+
     return (
-        <>
-            <h1>Admin Page</h1>
-            <h2>Hello there Admin</h2>
-        </>
+        <div className="admin__page container">
+            <Typography variant="h3">Admin Page</Typography>
+            <Typography variant="h5">Hello there Admin</Typography>
+
+            <ul>
+                {users && users.map((user, index) => {
+                    return <li key={index}>{user.data().username} {user.data().email} {user.id}</li>
+                })}
+            </ul>
+        </div>
     );
 };
 

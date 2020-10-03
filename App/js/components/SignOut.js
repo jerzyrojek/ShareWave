e5e4d1f0c21d2c156719186716288f57d3bd0419
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-10-03 17:25:16 +0200

    minor cleanup

--- App/js/components/SignOut.js
+++ App/js/components/SignOut.js
@@ -3,9 +3,23 @@ import {withFirebase} from "./Firebase/context";
 import {useHistory} from "react-router-dom";
 import * as ROUTES from "../constants/routes";
 import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        outline:"none",
+        border:"0",
+        display:"flex",
+        alignItems:"center",
+        backgroundColor:"#263238",
+        padding:"0 0.5rem",
+        fontSize:"1.3rem"
+    }
+}))
 
 const SignOut = (props) => {
 
+    const classes = useStyles();
     const history = useHistory();
 
     const handleClickSignOut = () => {
@@ -14,7 +28,7 @@ const SignOut = (props) => {
     };
 
     return (
-        <button style={{outline:"none", border:"0", display:"flex", alignItems:"center", backgroundColor:"#263238", padding:"0 1rem", fontSize:"1.3rem"}} onClick={handleClickSignOut}>
+        <button className={classes.button} onClick={handleClickSignOut}>
           <MeetingRoomIcon/>  Logout
         </button>
     );

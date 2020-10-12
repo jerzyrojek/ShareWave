dd3cc0be6efa5a4c8ae6cc958f5925552dd86668
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-10-12 18:16:35 +0200

    saving scrollY on unmount, scrolling to scrollY on back button

--- App/js/components/UserPostsMain.js
+++ App/js/components/UserPostsMain.js
@@ -1,10 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CardUserPost from "./CardUserPost";
 import Sidebar from "./Sidebar";
 import {withFirebase} from "./Firebase/context";
+import {useHistory} from "react-router-dom";
+import ScrollContext from "./ScrollContext";
 
 const UserPostsMain = (props) => {
     const [posts, setPosts] = useState(null);
+    const history = useHistory();
+    const scrollY = useContext(ScrollContext);
+
 
     useEffect(() => {
         let mounted = true;
@@ -15,11 +20,20 @@ const UserPostsMain = (props) => {
                     setPosts(querySnapshot.docs)
                 }
             })
+
         return () => {
+            scrollY.setNewScroll(window.scrollY);
             mounted = false;
         }
     },[]);
 
+    useEffect(() => {
+        if(history.action === "POP" && scrollY.scrollPosition > 500){
+            setTimeout(() => {
+                window.scrollTo(0, scrollY.scrollPosition);
+            },2000)
+        }
+    },[posts])
 
     return (
         <div className="app__body">

c630af70e453fc20c144d0f8dcede889576722b6
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-10-14 21:00:59 +0200

    early sidebar background and styling

--- App/js/components/Sidebar.js
+++ App/js/components/Sidebar.js
@@ -31,10 +31,33 @@ const useStyles = makeStyles((theme) => ({
     },
     drawerPaper: {
         width: drawerWidth,
+        background:"url(../../assets/background2.jpg) no-repeat center",
+        backgroundSize:"cover",
         top: "64px",
+        color:"white",
     },
     drawerContainer: {
+        position:"relative",
         overflow: 'auto',
+        height:"100%",
+        background:"rgba(0,0,0,0.7)"
+    },
+    icon: {
+      color:"white",
+    },
+    listItems: {
+        textAlign:"center",
+        "&:hover":{
+            background:"rgba(33,150,243,0.5)",
+        },
+    },
+    divider: {
+      backgroundColor:"rgba(255,255,255,0.5)",
+    },
+    addButton: {
+        "&:hover":{
+            background:"rgba(33,150,243,0.5)",
+        },
     },
     content: {
         flexGrow: 1,
@@ -58,8 +81,8 @@ const Sidebar = (props) => {
             <List>
                 <AuthUserContext.Consumer>
                     {authUser => authUser && authUser.role === "admin" ?
-                        <ListItem button onClick={handleClickAddCategory}>
-                            <ListItemIcon><AddBoxRoundedIcon/></ListItemIcon>
+                        <ListItem className={classes.addButton} button onClick={handleClickAddCategory}>
+                            <ListItemIcon ><AddBoxRoundedIcon className={classes.icon}/></ListItemIcon>
                             <ListItemText primary="Add a Category"/>
                         </ListItem>
                         :
@@ -67,14 +90,14 @@ const Sidebar = (props) => {
                     }
                 </AuthUserContext.Consumer>
 
-                <Divider/>
+                <Divider className={classes.divider}/>
                 <ListItem>
-                    <ListItemIcon><CategoryIcon/></ListItemIcon>
+                    <ListItemIcon ><CategoryIcon className={classes.icon}/></ListItemIcon>
                     <ListItemText primary="Categories"/>
                 </ListItem>
-                <Divider/>
+                <Divider className={classes.divider}/>
                 {categories && categories.map((category, index) => {
-                    return <ListItem button key={index} onClick={() => handleSelectCategory(category)}>
+                    return <ListItem className={classes.listItems} button key={index} onClick={() => handleSelectCategory(category)}>
                         <ListItemText primary={category.name}/>
                     </ListItem>
                 })

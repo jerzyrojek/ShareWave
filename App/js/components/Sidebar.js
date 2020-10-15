4de323f2bd0d37f986d621a7bf61212635052170
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-10-15 18:37:53 +0200

    styled scrollbar

--- App/js/components/Sidebar.js
+++ App/js/components/Sidebar.js
@@ -40,7 +40,18 @@ const useStyles = makeStyles((theme) => ({
         position:"relative",
         overflow: 'auto',
         height:"100%",
-        background:"rgba(0,0,0,0.7)"
+        background:"rgba(0,0,0,0.7)",
+        '&::-webkit-scrollbar': {
+            width: '0.4em'
+        },
+        '&::-webkit-scrollbar-track': {
+            boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+            webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+        },
+        '&::-webkit-scrollbar-thumb': {
+            backgroundColor: "rgba(33,150,243,0.7)",
+            outline: '1px solid slategrey'
+        }
     },
     icon: {
       color:"white",

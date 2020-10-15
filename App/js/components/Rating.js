599e40c7fddb5753ee38affd6fa60ca94391a502
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-10-15 17:43:14 +0200

    big rating overhaul

--- App/js/components/Rating.js
+++ App/js/components/Rating.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
@@ -11,97 +11,143 @@ const Rating = ({post, ...props}) => {
     const authUser = useContext(AuthUserContext);
     let currentUserId = authUser.uid
     const postRating = props.firebase.database.collection("posts").doc(post.id).collection("rating");
-    const [toggle, setToggle] = useState(false);
+    const [likedToggle, setLikedToggle] = useState(false);
+    const [dislikedToggle, setDislikedToggle] = useState(false);
     const [currentVote, setCurrentVote] = useState(0);
-    const [currentRating, setCurrentRating] = useState(null);
+    const [currentRating, setCurrentRating] = useState(0);
+    const isMountedRefLike = useRef(false);
+    const isMountedRefDislike = useRef(false);
 
 
     useEffect(() => {
+        console.log("fire every time");
         let mounted = true;
-        postRating.onSnapshot((snapshot) => {
+        postRating.onSnapshot(snapshot => {
             let currentCount = 0;
-            snapshot.forEach(doc => {
-                    currentCount += doc.data().state;
-                if(mounted) {
+            snapshot.docChanges().forEach(change => {
+                currentCount += change.doc.data().state;
+                if(mounted){
                     setCurrentRating(currentCount);
                 }
             })
+
+            const vote = snapshot.docs.filter((doc) => {
+                return doc.id === `${post.id}:${currentUserId}`
+            })
+
+            if(vote.length === 1 && mounted) {
+                setCurrentVote(vote[0].data().state);
+            }
         })
 
-
-
-        postRating.doc(`${post.id}:${currentUserId}`)
-            .onSnapshot(snapshot => {
-                if (snapshot.exists) {
-                    setCurrentVote(snapshot.data().state);
-                }
-            })
-            return () => {
+        return () => {
             mounted = false;
-            }
-    }, [])
-
+        }
+    }, []);
 
     useEffect(() => {
         let mounted = true;
-        postRating.onSnapshot((snapshot) => {
-            let currentCount = 0;
-            snapshot.forEach(doc => {
-                currentCount += doc.data().state;
-            });
+        if(isMountedRefLike.current) {
+            console.log("liked!")
+            if (currentVote === 1) {
+                postRating.doc(`${post.id}:${currentUserId}`).delete().then(() => {
+                    if(mounted) {
+                        setCurrentVote(0);
+                    }
+                })
+            } else{
+                postRating
+                    .doc(`${post.id}:${currentUserId}`)
+                    .set({
+                        state: 1,
+                    }).then(() => {
+                        if(mounted) {
+                            setCurrentVote(1);
+                        }
+                });
+            }
 
-            if (mounted) {
+            postRating.onSnapshot((snapshot) => {
+                let currentCount = 0;
+                snapshot.forEach(doc => {
+                    currentCount += doc.data().state;
+                });
+
                 props.firebase.database.collection("posts").doc(post.id).update({
                     rating: currentCount
                 }).then(() => {
-                    setCurrentRating(currentCount);
+                    if(mounted){
+                        setCurrentRating(currentCount);
+                    }
                 })
 
-            }
-        });
+            });
+
+        } else {
+            isMountedRefLike.current = true;
+        }
 
         return () => {
-            {
-                mounted = false;
-            }
-        };
+            mounted = false;
+        }
+    },[likedToggle]);
 
-    }, [toggle]);
+
+    useEffect(() => {
+        let mounted = true;
+        if(isMountedRefDislike.current) {
+            console.log("disliked!");
+            console.log(isMountedRefDislike);
+            if (currentVote === -1) {
+                postRating.doc(`${post.id}:${currentUserId}`).delete().then(() => {
+                    if(mounted) {
+                        setCurrentVote(0);
+                    }
+                })
+            }else{
+                postRating
+                    .doc(`${post.id}:${currentUserId}`)
+                    .set({
+                        state: -1,
+                    }).then(() => {
+                        if(mounted) {
+                            setCurrentVote(-1);
+                        }
+                });
+            }
+
+            postRating.onSnapshot((snapshot) => {
+                let currentCount = 0;
+                snapshot.forEach(doc => {
+                    currentCount += doc.data().state;
+                });
+
+                props.firebase.database.collection("posts").doc(post.id).update({
+                    rating: currentCount
+                }).then(() => {
+                    if(mounted){
+                        setCurrentRating(currentCount);
+                    }
+                })
+
+            });
+
+        } else {
+            isMountedRefDislike.current = true;
+        }
+
+        return () => {
+            mounted = false;
+        }
+    },[dislikedToggle]);
 
 
     const handleClickLiked = () => {
-        setToggle(prev => !prev);
-        if (currentVote === 1) {
-            postRating.doc(`${post.id}:${currentUserId}`).delete().then(() => {
-                setCurrentVote(0);
-            })
-        } else {
-            postRating
-                .doc(`${post.id}:${currentUserId}`)
-                .set({
-                    state: 1,
-                }).then(() => {
-                setCurrentVote(1);
-            });
-        }
+        setLikedToggle(prev => !prev);
     }
-
+    
     const handleClickDisliked = () => {
-        setToggle(prev => !prev);
-        if (currentVote === -1) {
-            postRating.doc(`${post.id}:${currentUserId}`).delete().then(() => {
-                setCurrentVote(0);
-            })
-        }else {
-            postRating
-                .doc(`${post.id}:${currentUserId}`)
-                .set({
-                    state: -1,
-                }).then(() => {
-                    setCurrentVote(-1);
-            });
-        }
-
+        setDislikedToggle(prev => !prev);
     }
 
     return (
